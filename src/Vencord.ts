77e3978df11263df45dc1ec2d2d1a872b9d3d2cd
Bapecord <mooonm309@gmail.com>
2025-08-21 01:10:19 -0400

    Vencord.ts

--- src/Vencord.ts
+++ src/Vencord.ts
@@ -1,5 +1,5 @@
 /*!
- * Bapecord, a modification for Discord's desktop app
+ * Vencord, a modification for Discord's desktop app
  * Copyright (c) 2022 Vendicated and contributors
  *
  * This program is free software: you can redistribute it and/or modify
